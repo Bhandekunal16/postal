5c1551da2c511353b31a2e341570224e6a2a7434
bhandekunal16 <bhandekunal16@gmail.com>
2024-03-11 13:16:17 +0530

    Add logging to print the value of the LOCALHOST environment variable.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const Logger = require('robotic.js/interface/Logger') as any;
+const logger = new Logger();
 require('dotenv').config();
 
+logger.log(process.env.LOCALHOST);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.LOCALHOST);
